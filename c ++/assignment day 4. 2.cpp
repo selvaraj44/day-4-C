3701b81fca2621b695df5fdbde037851760b8281
selvaraj44 <128613153+selvaraj44@users.noreply.github.com>
2023-03-23 07:45:43 +0530

    Add files via upload

---
+++ c ++/assignment day 4. 2.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include<iostream>
+int main() 
+{
+	int x=10;
+	int y=10;
+	
+	if(!(x^y))
+	cout<<"x is equal to y";
+	else
+	cout<<"x is not equal to y";
+}
---
+++ c ++/assignment day 4. 3.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include<iostream>
+int n=10;
+int main()
+{
+	static int x=1;
+	if(cout<<x<<"" && x++<n&& main())
+	{}
+	return 0;
+}
